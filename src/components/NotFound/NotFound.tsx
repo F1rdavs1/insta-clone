c699a7ddd966dd7320eeb82a786355c0fdcdd09a
F1rdavs5767 <firdavsistamow@gmail.com>
2024-10-24 13:50:56 +0500

    Initial commit

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound: React.FC = () => {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gradient-to-br from-blue-600 to-purple-600 text-white ">
+    <div className="min-h-screen w-[80%] flex flex-col items-center justify-center bg-gradient-to-br from-blue-600 to-purple-600 text-white ">
       <h1 className="text-[100px] font-extrabold">404</h1>
       <p className="text-[30px] mt-4">
         Oops! The page you're looking for doesn't exist.
